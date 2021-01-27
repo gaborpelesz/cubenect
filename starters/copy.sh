d0e316f40cf715290cb2cac4f4c450fcb7ab3744
Gabor <gaborpelesz@gmail.com>
2021-01-27 21:00:21 +0100

    remove unnecessary starter

--- starters/copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/digital/log_cubenect.out 2>&1
-sleep 10
-
-echo "Starting..."
-sudo python3 /home/digital/cubenect/cubenect/test_multitouch.py
