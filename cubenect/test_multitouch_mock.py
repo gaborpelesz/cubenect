644df94005ad0640e692bef9f982d9ad45b92ba5
Gabor <gaborpelesz@gmail.com>
2021-01-27 20:57:35 +0100

    refactor classes and scripts

---
+++ cubenect/test_multitouch_mock.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cubenect
+import multitouch_driver as mtd
+
+# action_processing/test/videos/record_close_1610737635.npy
+file_path = f"{'/'.join(__file__.split('/')[:4])}/action_processing/test/videos/record_close_1610737635.npy"
+
+with open(file_path, "rb") as f:
+    depth_video = np.load(f)
+
+driver = mtd.MultitouchDriver()
+
+cube = cubenect.Cubenect(dummy_loop_frames=depth_video, debug=False, dummy_loop_frames_n=1000)
+cube.run(contact_update_callback=driver.multitouch_contact_callback)
+print("exiting program")
--- cubenect/test_tracking.py
+++ cubenect/test_tracking.py
@@ -1,38 +1,7 @@
-import numpy as np
 import cubenect
-import tracker
+import multitouch_driver_mock as mock_mtd
 
-class InterfaceLogic:
-    def __init__(self):
-        self.current_commands = None 
-
-    def freeup_slot(self, slot_id):
-        self.current_commands.append(f"Free up slot: {slot_id}")
-
-    def activate_slot(self, slot_id):
-        self.current_commands.append(f"Lock slot: {slot_id}")
-
-    def move(self, slot_id, center):
-        self.current_commands.append(f"slot {slot_id}: move -> ({center[0]}, {center[1]})")
-
-    def print_action_callback(self, contact_tracker):
-        self.current_commands = []
-        for slot_id in range(len(contact_tracker.slots)):
-            if contact_tracker.slots[slot_id].was_freed:
-                self.freeup_slot(slot_id)
-                contact_tracker.slots[slot_id].was_freed = False
-            if contact_tracker.slots[slot_id].activate:
-                self.activate_slot(slot_id)
-                contact_tracker.slots[slot_id].activate = False
-            if not contact_tracker.slots[slot_id].center is None:
-                self.move(slot_id, contact_tracker.slots[slot_id].center)
-        self.send_commands()
-
-    def send_commands(self):
-        if len(self.current_commands) > 0:
-            print('\n'.join(self.current_commands))
-
-logic = InterfaceLogic()
+mock_driver = mock_mtd.MultitouchDriverMock()
 
 cube = cubenect.Cubenect(debug=True)
-cube.run(contact_update_callback=logic.print_action_callback)
+cube.run(contact_update_callback=mock_driver.print_action_callback)
