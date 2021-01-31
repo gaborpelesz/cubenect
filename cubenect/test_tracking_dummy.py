2fbd71b40356cd640d2e0e00c7733779bcfa1f8b
Gabor <gaborpelesz@gmail.com>
2021-01-31 21:09:19 +0100

    add new recorded video

--- cubenect/test_tracking_dummy.py
+++ cubenect/test_tracking_dummy.py
@@ -4,7 +4,9 @@ import multitouch_driver_mock as mock_mtd
 
 mock_driver = mock_mtd.MultitouchDriverMock()
 
+
 with open("action_processing/test/videos/record_close_1610737635.npy", "rb") as f:
+#with open("action_processing/test/videos/record_close_1612111434.npy", "rb") as f:
     depth_video = np.load(f)
 
 cube = cubenect.Cubenect(dummy_loop_frames=depth_video, debug=True, dummy_loop_frames_n=2000)
