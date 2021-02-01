8ff4daed326d41592fae17820d813f94331636a1
Gabor <gaborpelesz@gmail.com>
2021-02-01 12:43:39 +0100

    vertical flip to multitouch

--- cubenect/test_multitouch.py
+++ cubenect/test_multitouch.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import cubenect
 import multitouch_driver as mtd
+import utils
 
 print("initialize driver")
 driver = mtd.MultitouchDriver()
 print("driver initialized")
 
-print("starting cubenect in debug mode")
-cube = cubenect.Cubenect(debug=False)
+print("starting cubenect in production, with 90 degree rotated kinect")
+cube = cubenect.Cubenect(debug=False, flip=utils.CV2_VERTICAL_FLIP)
 print("running cubenect...")
 cube.run(contact_update_callback=driver.multitouch_contact_callback)
 print("exiting program")
